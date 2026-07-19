9c1733b7dd540877c06e9a4f6d374d3ab01ac9f9
bekam <bekwizgobisa321@gmail.com>
2026-07-19 09:54:41 -0300

    commitng the code but there is a problem ill open an issie and when i fix it i will close the issue but now commitnng what i wrote

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const taskInput = document.querySelector('#task-input');
-const addBtn = document.querySelector('#the-add-task-btn');
-const taskList = document.querySelector('.task-list');
-
+const taskInput = document.querySelector("#task-input");
+const addBtn = document.querySelector("#the-add-task-btn");
+const taskList = document.querySelector(".task-list");
 
 addBtn.addEventListener("click", function () {
-    const enteredText = taskInput.value;
-   if (enteredText.trim() === ''){
-    return;
-   }
+   const enteredText = taskInput.value;
    console.log(enteredText);
-     
-});
+  if (enteredText.trim() === "") {
+    return;
+  }
+  const li = document.createElement("li");
+  li.className = "T-item";
+  li.innerHTML = `
+  <input type="checkbox" class="task-check">
+  <span class="task-text">${enteredText}</span>
+  <button class="delete-btn" type="button" aria-label="Delete task">x</button>
+  `;
+   taskList.appendChild(li);
+   taskInput.value = "";
+  });
+
+  taskList.addEventListener("click", function (event){
+    if (event.target.classList.contains("delete-btn")){
+        console.log("delete button was clicked!")
+    }
+    const taskItem = event.target.parentElement;
+    taskItem.remove();
+  });
