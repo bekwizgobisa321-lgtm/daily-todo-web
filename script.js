6da9b9df0c859beaae3976d1c97069ecab5c39b5
bekam <bekwizgobisa321@gmail.com>
2026-07-19 15:19:46 -0300

    commit

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 const taskInput = document.querySelector("#task-input");
 const addBtn = document.querySelector("#the-add-task-btn");
 const taskList = document.querySelector(".task-list");
-
+/////
+let tasks = [];
+const savedTasks = localStorage.getItem("tasks");
+console.log(savedTasks);
+///
 addBtn.addEventListener("click", function () {
-   const enteredText = taskInput.value;
-   console.log(enteredText);
+  const enteredText = taskInput.value;
+
   if (enteredText.trim() === "") {
     return;
   }
+  const newTask = {
+    text: enteredText,
+    completed: false,
+  };
+  tasks.push(newTask);
+  console.log(tasks);
+  //
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  //'
   const li = document.createElement("li");
   li.className = "T-item";
   li.innerHTML = `
-  <input type="checkbox" class="task-check">
-  <span class="task-text">${enteredText}</span>
-  <button class="delete-btn" type="button" aria-label="Delete task">x</button>
-  `;
-   taskList.appendChild(li);
-   taskInput.value = "";
-  });
+        <input type="checkbox" class="task-check">
+        <span class="task-text">${enteredText}</span>
+        <button class="delete-btn" type="button" aria-label="Delete task">x</button> `;
+  taskList.appendChild(li);
+  taskInput.value = "";
+});
 
-  taskList.addEventListener("click", function (event){
-    if (event.target.classList.contains("delete-btn")){
-        console.log("delete button was clicked!")
-    }
+taskList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-btn")) {
+    console.log("delete button was clicked!");
+
     const taskItem = event.target.parentElement;
     taskItem.remove();
-  });
+  }
+
+ if (event.target.classList.contains("task-check")) {
+   const taskText = event.target.nextElementSibling;
+   taskText.classList.toggle("completed");
+ }
+
+});
+
